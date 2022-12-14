a4e4c98c2a2a2e69686580714a5595026363ccf4
Martin Grillo <martine.grillo@gmail.com>
2022-12-14 09:03:27 -0300

    modified:   .DS_Store 	deleted:    public/.DS_Store 	deleted:    public/css/.DS_Store 	modified:   public/css/styles.css 	modified:   public/index.html 	modified:   public/main.js

--- public/main.js
+++ public/main.js
@@ -3,27 +3,28 @@ var horas = document.getElementById("horas");
 var minutos = document.getElementById("minutos");
 var segundos = document.getElementById("segundos");
 // SE CUENTA HACIA LA FECHA APROXIMADAD DE LA REALIZACIN DEL EVENTO
-var countDownDate = new Date("Mar 18, 2022 00:00:00").getTime();
+var countDownDate = new Date("Mar 17, 2023 00:00:00").getTime();
 
 // ACTUALIZAR EL CONTADOR CADA 1 SEGUNDO
 
-var x = setInterval(function() {
-    // TOMA LA FECHA DE HOY
-    var now = new Date().getTime();
-    // ENCENTRA DISTANCIA ENTRE LA FECHA DEL EVENTO Y LA FECHA DE HOY 
-    var distance = countDownDate - now;
+var x = setInterval(function () {
+	// TOMA LA FECHA DE HOY
+	var now = new Date().getTime();
+	// ENCENTRA DISTANCIA ENTRE LA FECHA DEL EVENTO Y LA FECHA DE HOY
+	var distance = countDownDate - now;
 
-    // CALCULA DIAS, HORAS, MINUTOS Y SEGUNDOS
+	// CALCULA DIAS, HORAS, MINUTOS Y SEGUNDOS
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) /(1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60))/ 1000);
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    dias.innerHTML = days;
-    horas.innerHTML = hours;
-    minutos.innerHTML = minutes;
-    segundos.innerHTML = seconds;
-    
-
-})
+	dias.innerHTML = days;
+	horas.innerHTML = hours;
+	minutos.innerHTML = minutes;
+	segundos.innerHTML = seconds;
+	if (countDownDate === now) {
+		alert("Lollapalooza AR 2023 ya paso ");
+	}
+}, 1000);
